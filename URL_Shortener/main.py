5340d02bd16abe405338dd74bf226bcea68d51be
18arihant <arihant18082003@gmail.com>
2023-10-23 13:07:41 +0530

    update

--- URL_Shortener/main.py
+++ URL_Shortener/main.py
@@ -4,22 +4,22 @@ from tkinter import *
 win = Tk()
 win.geometry("400x270")
 win.configure(bg="pink")
-# button function
+
+# Button function
 def short():
     url = entry1.get()
     s=pyshorteners.Shortener().tinyurl.short(url)
 
     entry2.insert(END,s)
-    
-    
-#label
+       
+# Label
 Label(win,text="Enter your URL Link",font="impack 17 bold", bg="black", fg="white").pack(fill="x")
-#Entry box
+# Entry box
 entry1= Entry(win,font="10",bd=3,width=30)
 entry1.pack(pady=30)
-#Button
+# Button
 Button(win, text="submit",font="impact 10 ",bg="blue",fg="white",width="14",command=short).pack()
-#Entry
+# Entry
 entry2=Entry(win,font="impact 16 bold", bg="pink",width=24,bd=0)
 entry2.pack(pady=30)
 mainloop()
