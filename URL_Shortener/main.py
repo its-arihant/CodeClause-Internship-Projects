809a53b1a5d2bc760f10dafe02197249437baa90
18arihant <arihant18082003@gmail.com>
2023-10-23 12:31:17 +0530

    update

--- URL_Shortener/main.py
+++ URL_Shortener/main.py
@@ -5,6 +5,6 @@ win = Tk()
 win.geometry("400x270")
 win.configure(bg="pink")
 #label
-
+Label(win,text="Enter your URL Link",font="impack 17 bold", bg="black", fg="white").pack(fill="x")
 mainloop()
 
