f768c76d1fdc4f98bc31a89493a52dd2300bfcd9
18arihant <arihant18082003@gmail.com>
2023-10-23 13:01:19 +0530

    update

--- URL_Shortener/main.py
+++ URL_Shortener/main.py
@@ -4,13 +4,17 @@ from tkinter import *
 win = Tk()
 win.geometry("400x270")
 win.configure(bg="pink")
+# button function
+def short():
+    pass
+    
 #label
 Label(win,text="Enter your URL Link",font="impack 17 bold", bg="black", fg="white").pack(fill="x")
 #Entry box
 entry1= Entry(win,font="10",bd=3,width=30)
 entry1.pack(pady=30)
 #Button
-Button(win, text="submit",font="impact 10 ",bg="blue",fg="white",width="14").pack()
+Button(win, text="submit",font="impact 10 ",bg="blue",fg="white",width="14",command=short).pack()
 #Entry
 entry2=Entry(win,font="impact 16 bold", bg="pink",width=24,bd=0)
 entry2.pack(pady=30)
